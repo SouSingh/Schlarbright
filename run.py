39a2a789da6686fba9345d4aefda537e75346864
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2024-07-13 11:54:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from typing import List, Optional, Union, Dict, Any
 import json
 from test import details
+import uvicorn
 
 app = FastAPI()
 
@@ -25,6 +26,9 @@ async def Agent1(request: PromptRequest):
     prompt = request.prompt
     search_result = details(prompt)
     return search_result
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 # prompt = "I am X, a second year UG in iiit manipur .Give me the scholarships I'm eligible for."
 # details(prompt)
